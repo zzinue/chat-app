45987d63045cbcececcc66d88e0659ee8522940d
zzinue <zzinue@gmail.com>
2024-08-15 08:53:01 -0600

    adding chat left side and right side pages

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import './Chat.css'
+import React from "react";
+import "./Chat.css";
+import LeftSidebar from "../../components/LeftSidebar/LeftSidebar";
+import ChatBox from "../../components/ChatBox/ChatBox";
+import RightSidebar from "../../components/RightSidebar/RightSidebar";
 const Chat = () => {
   return (
-    <div>We are on the chat</div>
-  )
-}
+    <div className="chat">
+      <div className="chat-container">
+        <LeftSidebar />
+        <ChatBox />
+        <RightSidebar />
+      </div>
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
